d194fdbbb0a0adacb4263bd0a3d32729653a320c
Eric Guerrero Font <ericguerrerofont@gmail.com>
2017-11-10 14:54:09 +0100

    Publish descriptor array working

--- src/pod/include/podnode.h
+++ src/pod/include/podnode.h
@@ -30,9 +30,10 @@ private:
   ros::NodeHandle _nh;              // Node handler copy.
   image_transport::ImageTransport _it;  // Required for image messages
   image_transport::Subscriber _sub; // Subscriber to input images
-  ros::Publisher _pub_desc;  // Publishes descriptor matrix
+  ros::Publisher _pub_descriptor;  // Publishes descriptor matrix
   image_transport::Publisher _pub_class;  // Publishes classified images
   image_transport::Publisher _pub_image;  // Publishes classified images
+  float _imageDescriptor[_POD_NPATCH_ROW_][_POD_NPATCH_COL_][_POD_DESCR_TYPES_][_POD_IMAGE_NCHAN_][_POD_GABOR_SCALES_][_POD_GABOR_ORIENT_];
   int _overlay;                     // Output selector
   // The image message callback.
   void processImage(const sensor_msgs::ImageConstPtr& msg);
--- src/pod/src/podnode.cpp
+++ src/pod/src/podnode.cpp
@@ -8,7 +8,7 @@ PODNode::PODNode(const ros::NodeHandle& nh,string configPath,int overlay):_nh(nh
   _sub=_it.subscribe("image_in", 1, &PODNode::processImage,this);
 
   //Set publishers; descriptors, overlayed classification, and binary classification.
-  _pub_desc=_nh.advertise<std_msgs::Float64MultiArray>("descriptors", 1);
+  _pub_descriptor=_nh.advertise<std_msgs::Float64MultiArray>("descriptors", 10);
   _pub_image=_it.advertise("image_out", 1);
   _pub_class=_it.advertise("classification", 1);
 }
@@ -27,13 +27,22 @@ void PODNode::processImage(const sensor_msgs::ImageConstPtr& msg) {
   _theDetector->set_image(cv_bridge::toCvShare(msg, "bgr8")->image);
   _theDetector->classify_image();
 
-  // Get patch descriptors, overlayed classification, and binary classification
+  // Get patch descriptors
   pDescriptor=_theDetector->get_descriptor();
+  int size = _POD_NPATCH_ROW_*_POD_NPATCH_COL_*_POD_DESCR_TYPES_*_POD_IMAGE_NCHAN_*_POD_GABOR_SCALES_*_POD_GABOR_ORIENT_;
+  std_msgs::Float64MultiArray descriptor;
+  descriptor.data.clear();
+  for (int i = 0; i < size; ++i)
+  {
+    descriptor.data.push_back(*(pDescriptor+i));
+  }
+
+  // Get overlayed classification, and binary classification
   outMat=_theDetector->get_binary_classification();
   outImage=_theDetector->get_graphic_classification();
-
-
+  
   // Publish
+  _pub_descriptor.publish(descriptor);
   _pub_image.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", outImage).toImageMsg());
   _pub_class.publish(cv_bridge::CvImage(std_msgs::Header(), "mono8", outMat).toImageMsg());
 }
